ca46f513455a689de26f71bec4effbb7cb7e03c5
David Jose Rodriguez Arce <drodriguez30407@ufide.ac.cr>
2025-12-11 22:39:18 -0600

    tienda v1
    practicas

---
+++ src/main/java/com/tienda/service/CategoriaService.java
@@ -0,0 +1,53 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+
+package com.tienda.service;
+
+import com.tienda.domain.Categoria;
+import com.tienda.repository.CategoriaRepository;
+import java.util.List;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+/**
+ *
+ * @author davidrodriguezarce
+ */
+@Service
+public class CategoriaService {
+
+    @Autowired
+    private CategoriaRepository categoriaRepository;
+
+    @Transactional(readOnly = true)
+    public List<Categoria> getCategorias(boolean activo) {
+        var lista = categoriaRepository.findAll();
+        if (activo) {
+            lista.removeIf(e -> !e.getActivo());
+        }
+        return lista;
+    }
+    @Transactional
+    public void save(Categoria categoria){
+        categoriaRepository.save(categoria);
+    }
+    @Transactional
+    public boolean delete(Categoria categoria){
+        try {
+            categoriaRepository.delete(categoria);
+            categoriaRepository.flush();
+            return true;
+            
+        } catch (Expetion e) {
+            return false;
+        }
+        
+        @Transactional(readOnly = true)
+            public Categoria getCategoria(Categoria categoria){ 
+            return categoriaRepository.findById(categoria.getIdCategoria()).orElse(null);
+        }
+    }
+}
